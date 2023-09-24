1d831f40010931b7dc84eceb45c727b2ce74513a
Paweł Janowicz <JanowiczPawel4@gmail.com>
2023-09-24 13:48:07 +0200

    Complete Kata1

--- src/main/java/Kata1.java
+++ src/main/java/Kata1.java
@@ -1,2 +1,10 @@
+//Implement a function which convert the given boolean value into its string representation.
 public class Kata1 {
+    public static String convert(boolean b) {
+        if (b == true) {
+            return "True";
+        } else {
+            return "False";
+        }
+    }
 }
