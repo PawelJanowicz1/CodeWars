1b5ed3f994a33cc20f79496de0141140ab1e090a
Paweł Janowicz <JanowiczPawel4@gmail.com>
2023-09-24 17:16:08 +0200

    Complete Kata8

---
+++ src/main/java/Kata8.java
@@ -0,0 +1,13 @@
+/* You receive an array with your peers' test scores. Now calculate the average and compare your score!
+Return True if you're better, else False!
+ */
+public class Kata8 {public static boolean betterThanAverage(int[] classPoints, int yourPoints) {
+    int totalScore = 0;
+    for(int score : classPoints){
+        totalScore =+ score;
+    }
+    totalScore =+ yourPoints;
+    double average = (double)totalScore / (classPoints.length + 1);
+    return yourPoints > average;
+}
+}
