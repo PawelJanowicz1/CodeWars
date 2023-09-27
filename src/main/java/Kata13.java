dbb56af65ead461e3ce355cdfd8fda0c260ea034
Paweł Janowicz <JanowiczPawel4@gmail.com>
2023-09-27 18:16:38 +0200

    Complete Kata13

---
+++ src/main/java/Kata13.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+/*Write a function which calculates the average of the numbers in a given list.
+        Note: Empty arrays should return 0.
+ */
+public class Kata13 {
+    public static double find_average(int[] array) {
+       return Arrays.stream(array).average().orElse(0);
+    }
+}
