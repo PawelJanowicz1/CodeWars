b5c73963e68da3f418ba45f9868493cdc0243e0b
Paweł Janowicz <JanowiczPawel4@gmail.com>
2023-09-27 18:57:23 +0200

    Complete Kata14

--- src/main/java/Kata14.java
+++ src/main/java/Kata14.java
@@ -1,2 +1,22 @@
+import java.sql.PreparedStatement;
+
+/*Given a string indicating a range of letters,
+ return a string which includes all the letters in that range, including the last letter.
+  Note that if the range is given in capital letters, return the string in capitals also!
+
+ */
 public class Kata14 {
+    public static String gimmeTheLetters(String s) {
+        if (s == null || s.length() < 3 || s.charAt(1) != '-') {
+            return "";
+        }
+        char start = s.charAt(0);
+        char end = s.charAt(2);
+        StringBuilder sb = new StringBuilder();
+
+        for (char c = start; c <= end; c++) {
+                sb.append(c);
+        }
+return sb.toString();
+    }
 }
