46b0a6c64f665da67d9e756b12efc5794dc14bb9
Paweł Janowicz <janowiczpawel4@gmail.com>
2023-11-07 08:12:06 +0100

    Complete Kata21

--- src/main/java/Kata21.java
+++ src/main/java/Kata21.java
@@ -1,6 +1,15 @@
 import java.util.Map;
 import java.util.HashMap;
+/* Your task
+is to write a function,
+which will find the number of zeroes at the end of (number) factorial in arbitrary radix = base for larger numbers.
 
+base is an integer from 2 to 256
+number is an integer from 1 to 1'000'000
+Note Second argument: number is always declared, passed and displayed as a regular decimal number.
+If you see a test described as 42! in base 20 it's 4210 not 4220 = 8210.
+
+*/
 public class Kata21 {
     public static int zeroes(int base, int number) {
         Map<Integer, Integer> baseFactors = primeFactorization(base);
