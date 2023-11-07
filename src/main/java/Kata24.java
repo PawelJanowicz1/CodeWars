ef4a50f90307073cd59bd8b8b4c3ac5bb93a15df
Paweł Janowicz <janowiczpawel4@gmail.com>
2023-11-07 08:51:16 +0100

    Complete Kata24

---
+++ src/main/java/Kata24.java
@@ -0,0 +1,7 @@
+/* Write a function that removes the spaces from the string, then return the resultant string.*/
+public class Kata24 {
+    public static String noSpace(final String x) {
+        return x.replace(" ", "");
+    }
+}
+
