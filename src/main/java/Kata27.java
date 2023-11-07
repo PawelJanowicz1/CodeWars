f3f1eed5ad37a18669b0b5946475f34fb1e51b39
Paweł Janowicz <janowiczpawel4@gmail.com>
2023-11-07 13:59:56 +0100

    Complete Kata27

---
+++ src/main/java/Kata27.java
@@ -0,0 +1,8 @@
+/* Many programming languages provide the functionality of converting
+a string to uppercase or lowercase. For example, upcase/downcase in Ruby,
+upper/lower in Python, and toUpperCase/toLowerCase in Java/JavaScript,
+uppercase/lowercase in Kotlin. Typically, these methods won't change the size of the string. */
+public class Kata27 {
+    static final String STRANGE_STRING = "ß";
+}
+
